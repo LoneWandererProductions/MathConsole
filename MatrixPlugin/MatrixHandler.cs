14c8f5a59853db1602f9e0a2e9b3cf41ca1a2e87
Wayfarer <petergeinitz@freenet.de>
2024-06-04 21:37:20 +0200

    add some stuff

--- MatrixPlugin/MatrixHandler.cs
+++ MatrixPlugin/MatrixHandler.cs
@@ -1,8 +1,47 @@
-﻿using System;
+﻿using Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace MatrixPlugin
 {
-    public class MatrixHandler
+    public static class MatrixHandler
     {
+        private static bool HandleCommands(OutCommand outCommand)
+        {
+            //TODO add switch for Namespace
+
+            switch (outCommand.Command)
+            {
+                case 0:
+                    return SetMatrix(outCommand.Parameter);
+                case 1:
+                    //TODO
+                    break;
+
+                case 2:
+                    //TODO
+                    break;
+
+                case 3:
+                    //TODO
+                    break;
+
+                case 4:
+                    //TODO
+                    break;
+
+                default:
+                    return false;
+            }
+
+            return false;
+        }
+
+
+        public static bool SetMatrix(List<string> parameter)
+        {
+            return false;
+        }
     }
 }
