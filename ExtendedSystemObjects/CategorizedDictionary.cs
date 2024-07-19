22b7480a1d25b0f0717caaec079589b20bb6c44b
Wayfarer <petergeinitz@freenet.de>
2024-07-19 19:48:36 +0200

    sync extended

---
+++ ExtendedSystemObjects/CategorizedDictionary.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     ExtendedSystemObjects
+ * FILE:        ExtendedSystemObjects/CategorizedDictionary.cs
+ * PURPOSE:     Extended Dictionary with an Category.
+ * PROGRAMER:   Peter Geinitz (Wayfarer)
+ */
+
+// ReSharper disable UnusedMethodReturnValue.Global
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtendedSystemObjects
+{
+    /// <summary>
+    /// Dictionary with an category
+    /// </summary>
+    /// <typeparam name="K">Key Value</typeparam>
+    /// <typeparam name="V">Value with Category</typeparam>
+    public class CategorizedDictionary<K, V>
+    {
+        /// <summary>
+        /// The internal data of our custom Dictionary
+        /// </summary>
+        private readonly Dictionary<K, (string Category, V Value)> _data = new Dictionary<K, (string Category, V Value)>();
+
+        /// <summary>
+        /// Adds a value to the dictionary under the specified category.
+        /// </summary>
+        /// <param name="category">The category under which to add the key-value pair. Can be null.</param>
+        /// <param name="key">The key of the value to add.</param>
+        /// <param name="value">The value to add.</param>
+        public void Add(string category, K key, V value)
+        {
+            _data[key] = (category, value);
+        }
+
+        /// <summary>
+        /// Gets a value from the dictionary based on the key.
+        /// </summary>
+        /// <param name="key">The key of the value to get.</param>
+        /// <returns>The value if found, otherwise the default value for the type.</returns>
+        public V Get(K key)
+        {
+            return _data.TryGetValue(key, out var entry) ? entry.Value : default(V);
+        }
+
+        /// <summary>
+        /// Gets the category and value from the dictionary based on the key.
+        /// </summary>
+        /// <param name="key">The key of the value to get.</param>
+        /// <returns>A tuple containing the category and value if found, otherwise null.</returns>
+        public (string Category, V Value)? GetCategoryAndValue(K key)
+        {
+            return _data.TryGetValue(key, out var entry) ? entry : ((string, V)?)null;
+        }
+
+        /// <summary>
+        /// Gets all key-value pairs under the specified category.
+        /// </summary>
+        /// <param name="category">The category to retrieve values from. Can be null.</param>
+        /// <returns>A dictionary of key-value pairs in the specified category.</returns>
+        public Dictionary<K, V> GetCategory(string category)
+        {
+            return _data
+                .Where(entry => entry.Value.Category == category)
+                .ToDictionary(entry => entry.Key, entry => entry.Value.Value);
+        }
+
+        /// <summary>
+        /// Gets all categories.
+        /// </summary>
+        /// <returns>An enumerable of all categories.</returns>
+        public IEnumerable<string> GetCategories()
+        {
+            return _data.Values
+                .Select(entry => entry.Category)
+                .Distinct();
+        }
+    }
+}
